25755e38a4af379e65cb1436cb8224a2d94457c8
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-14 15:19:59 +0100

    Add files via upload

---
+++ Cwiczenia 5 Zad 6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Cwiczenia_5_Zad_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0, suma2 = 0;
+            byte[,] liczby =
+            {
+                {1, 2, 3, 4, 4},
+                {2, 2, 2, 2, 2},
+                {2, 4, 100, 8, 1},
+                {1, 3, 5, 7, 9},
+                {9, 8, 7, 6, 5}
+            };
+
+            for (int i = 0; i <= 4; i++)
+            {
+                suma += liczby[i, i];
+                suma2 += liczby[i, 4-i];
+            }
+            Console.WriteLine("Suma = " + suma + "\tSuma2 = " + suma2);
+        }
+    }
+}
